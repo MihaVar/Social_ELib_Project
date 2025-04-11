b9fc0bce80284007bd10189b336b7dd0bca110fa
MihaVar <mykhailvarych@gmail.com>
2025-04-11 10:04:34 +0300

    init commit

---
+++ src/main/java/org/mvar/social_elib_project/SocialELibProjectApplication.java
@@ -0,0 +1,13 @@
+package org.mvar.social_elib_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialELibProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialELibProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mvar/social_elib_project/SocialELibProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mvar.social_elib_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialELibProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
