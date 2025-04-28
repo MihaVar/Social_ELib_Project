3bb5eabda36b3176ffb5d454bb60ce0094301836
MihaVar <mykhailvarych@gmail.com>
2025-04-28 22:38:37 +0300

    CORS adjustment

--- src/main/java/org/mvar/social_elib_project/config/CorsConfig.java
+++ src/main/java/org/mvar/social_elib_project/config/CorsConfig.java
@@ -1,17 +1,23 @@
 package org.mvar.social_elib_project.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000"
-                );
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // дозволити для всіх ендпоінтів
+                        .allowedOrigins("http://localhost:3000") // твій фронтенд адрес
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
